55e6d84e6084f33f693571b6c2a93bb8ffde05e8
Amani200 <74149933+Amani200@users.noreply.github.com>
2024-03-11 09:27:46 +0300

    Delete phone directory

--- phone/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Items,ItemDetails,Itemsipad,ItemDetailsipad
-
-# Register your models here.
-admin.site.register(Items)
-admin.site.register(ItemDetails)
-admin.site.register(Itemsipad)
-admin.site.register(ItemDetailsipad)
-
