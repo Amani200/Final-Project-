fed85db6fca49d3c908cee56cb26ac9e4fcb3048
Amani200 <74149933+Amani200@users.noreply.github.com>
2024-03-11 09:46:26 +0300

    Add files via upload

---
+++ phone/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Items,ItemDetails,Itemsipad,ItemDetailsipad
+
+# Register your models here.
+admin.site.register(Items)
+admin.site.register(ItemDetails)
+admin.site.register(Itemsipad)
+admin.site.register(ItemDetailsipad)
+
